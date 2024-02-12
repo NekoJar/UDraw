2cf7030a57cadf2cd9d84d2a497db500bac1cbe1
NekoJar <mzharfanhakim7@gmail.com>
2024-02-12 23:26:26 +0700

    creating sidebar

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { NewButton } from "./NewButton";
+import { List } from "./List";
 
 export const Sidebar = () => {
   return (
     <aside className="fixed z-[1]left-0 bg-blue-950 h-full w-[60px] flex flex-col p-3 gap-y-4 text-white">
-      Side
+      <List />
+      <NewButton />
     </aside>
   );
 };
