f14cc5701fa706174514f7de462a8b34ed40feb2
NekoJar <mzharfanhakim7@gmail.com>
2024-02-22 00:02:37 +0700

    fixing minor bug

--- app/(dashboard)/_components/BoardList.tsx
+++ app/(dashboard)/_components/BoardList.tsx
@@ -25,7 +25,14 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
     return (
       <div>
         <h2>{query.favorites ? "Favorite boards" : "Team boards"}</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10"><NewBoardButton orgId={orgId} disabled</div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
       </div>
     );
   }
