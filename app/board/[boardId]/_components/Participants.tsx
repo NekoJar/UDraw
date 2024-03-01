cc290e3a95eccf3dfb206e7da3010277b58a9beb
NekoJar <mzharfanhakim7@gmail.com>
2024-03-01 23:19:48 +0700

    setting up liveblocks

--- app/board/[boardId]/_components/Participants.tsx
+++ app/board/[boardId]/_components/Participants.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-
 export const Participants = () => {
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
       Participants
     </div>
+  );
+};
+
+Participants.Skeleton = function ParticipantsSkeleton() {
+  return (
+    <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]" />
   );
 };
