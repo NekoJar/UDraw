351d378b592cd3895e7b1df60e19db5cf475e29b
NekoJar <mzharfanhakim7@gmail.com>
2024-03-05 00:14:11 +0700

    creating room info

--- app/board/[boardId]/_components/Loading.tsx
+++ app/board/[boardId]/_components/Loading.tsx
@@ -1,17 +1,15 @@
-import { Skeleton } from "@/components/ui/skeleton";
 import { Loader } from "lucide-react";
-import React from "react";
-import { Info } from "./Info";
-import { Participants } from "./Participants";
-import { Toolbar } from "./Toolbar";
+import { InfoSkeleton } from "./Info";
+import { ParticipantsSkeleton } from "./Participants";
+import { ToolbarSkeleton } from "./Toolbar";
 
 export const Loading = () => {
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none flex items-center justify-center">
       <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
-      <Info.Skeleton />
-      <Participants.Skeleton />
-      <Toolbar.Skelton />
+      <InfoSkeleton />
+      <ParticipantsSkeleton />
+      <ToolbarSkeleton />
     </main>
   );
 };
