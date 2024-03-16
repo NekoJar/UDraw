b5b7218b8f0618688a76db76e44e3c1bba3e5924
NekoJar <mzharfanhakim7@gmail.com>
2024-03-16 17:24:04 +0700

    feat: adding ellipse, note, and text

--- app/board/[boardId]/_components/LayerPreview.tsx
+++ app/board/[boardId]/_components/LayerPreview.tsx
@@ -4,6 +4,9 @@ import { useStorage } from "@/liveblocks.config";
 import { LayerType } from "@/types/canvas";
 import React, { memo } from "react";
 import { Rectangle } from "./Rectangle";
+import { Ellipse } from "./Ellipse";
+import { Text } from "./Text";
+import { Note } from "./Note";
 
 interface LayerPreviewProps {
   id: string;
@@ -20,6 +23,33 @@ export const LayerPreview = memo(
     }
 
     switch (layer.type) {
+      case LayerType.Note:
+        return (
+          <Note
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Text:
+        return (
+          <Text
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Ellipse:
+        return (
+          <Ellipse
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
       case LayerType.Rectangle:
         return (
           <Rectangle
